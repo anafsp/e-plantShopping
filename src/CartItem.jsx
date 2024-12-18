0595613847d9310103df98740a4c14788bcd57f5
anafsp <afilipaspereira@hotmail.com>
2024-12-18 13:05:37 -0500

    Update Cart item - Task 3 - 1 to 6

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,35 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.items.reduce(item => item.quantity * item.cost);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
-
+  const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item, item.quantity));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity <= 0)
+        dispatch(removeItem(item));
+    else
+        dispatch(updateQuantity(item, item.quantity));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * item.cost;
   };
 
   return (
@@ -57,7 +65,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
